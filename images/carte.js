7a8e746bb9e79f7b355fa8a929e01a1f64e66726
--replace-all <yabadalois.ly@gmail.com>
2021-11-08 17:30:27 +0100

    projet bootstratp

---
+++ images/carte.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    // alert ("Jquery fonctionne");
+
+    // $("#carte section").pretendTo(funtion()){
+
+
+};
+$("#carte section").click(function (){
+    $(this).prependTo("#carte");
+});
+    
+
+
+});
